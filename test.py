6539a8bb3740c63019794f00eda7f262cd1278bb
thaliawoods <thaliadelano@gmail.com>
2024-02-22 13:17:55 +0100

    CheckMove/Win/Play

--- test.py
+++ test.py
@@ -18,3 +18,56 @@ def print_grid(grid):
 
 print_grid(grid)
 
+
+# check the move
+def is_valid_move(grid, row, col, num):
+    # check the line
+    if num in grid[row]:
+        return False
+
+    # check the column
+    if num in [grid[i][col] for i in range(9)]:
+        return False
+
+    # check 3x3
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(start_row, start_row + 3):
+        for j in range(start_col, start_col + 3):
+            if grid[i][j] == num:
+                return False
+
+    return True
+
+# check if win
+def is_game_over(grid):
+    for row in grid:
+        if 0 in row:
+            return False
+    return True
+
+# user
+def main():
+    # print grid
+    print("Grille initiale:")
+    print_grid(grid)
+
+    while not is_game_over(grid):
+        # input user
+        row = int(input("Number of the line (1-9) : ")) - 1
+        col = int(input("Number of the column (1-9) : ")) - 1
+        num = int(input("Number (1-9) : "))
+
+        # check move
+        if is_valid_move(grid, row, col, num):
+            grid[row][col] = num
+            print("Okay")
+            print_grid(grid)
+        else:
+            print("Try again")
+
+    print("Congratulations !")
+
+if __name__ == "__main__":
+    main()
+
+
