f1687dcdff3a924479d34e3272c8fa7f1febdb4e
thaliawoods <thaliadelano@gmail.com>
2024-02-22 12:40:12 +0100

    CreateGrid/Print

---
+++ test.py
@@ -0,0 +1,20 @@
+# create the grid
+grid = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+# print the grid
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(str(num) if num != 0 else "." for num in row))
+
+print_grid(grid)
+
